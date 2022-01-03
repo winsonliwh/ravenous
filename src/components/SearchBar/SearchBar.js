f79704ff2f65efd91a1fa25264f14546cd93cdd6
Winson Li <winsonliwh@gmail.com>
2022-01-03 22:53:25 +0800

    Complete ravenous 49/49

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -7,13 +7,30 @@ const sortByOptions = {
   "Most Reviewed": "review_count",
 };
 
-class SearchBar extends React.Component{
-    renderSortByOptions() {
-        
-    }
-    render() {
-        return (
-
-        );
-    }
+class SearchBar extends React.Component {
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOption}>{sortByOptionValue}</li>;
+    });
+  }
+  render() {
+    return (
+      <div className="SearchBar">
+        <div className="SearchBar-sort-options">
+          <ul>{this.renderSortByOptions()}</ul>
+        </div>
+        <div className="SearchBar-fields">
+          <input placeholder="Search Businesses" />
+          <input placeholder="Where?" />
+        </div>
+        <div className="SearchBar-submit">
+          // eslint-disable-next-line jsx-a11y/anchor-is-valid
+          <a>Let's Go</a>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default SearchBar;
