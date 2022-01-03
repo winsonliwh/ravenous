a121b7c157e1efd1e79be718287ec9aa25e25f85
Winson Li <winsonliwh@gmail.com>
2022-01-03 20:27:54 +0800

    Complete ravenous project 33/49

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import "./BusinessList.css";
+import Business from "../Business/Business";
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        <Business />
+        <Business />
+        {/* Add four more Business components} */}
+        <Business />
+        <Business />
+        {/* Add four more Business components} */}
+        <Business />
+        <Business />
+        {/* Add four more Business components} */}
+        <Business />
+        <Business />
+        {/* Add four more Business components} */}
+        <Business />
+        <Business />
+        {/* Add four more Business components} */}
+        <Business />
+        <Business />
+        {/* Add four more Business components} */}
+      </div>
+    );
+  }
+}
+
+
+export default BusinessList;
---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./SearchBar.css";
+
+const sortByOptions = {
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
+};
+
+class SearchBar extends React.Component{
+    renderSortByOptions() {
+        
+    }
+    render() {
+        return (
+
+        );
+    }
+}
