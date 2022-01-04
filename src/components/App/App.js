e8d06fd683461d80ac8f2fdee647c488c7735904
Winson Li <winsonliwh@gmail.com>
2022-01-04 22:37:16 +0800

    Add fix of S099 and merge b.0.0.2.1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,11 +7,27 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          to
-          Edit <code>src/App.js</code> and save to reload!!!
-          because of New Year 2021 Event
-            Hot fixing New Year 2021 Event S099
+          Edit <code>src/App.js</code> and save to reload!!! Happy New Year 2021 Event!
+          Created New Feature Branch
+            with fixing New Year 2021 Event S099
+            >>>>>>>>>>
+              to
+              Edit <code>src/App.js</code> and save to reload!!!
+              because of New Year 2021 Event
+                Hot fixing New Year 2021 Event S099
+            >>>>>>>>> End of Hot fixing
+          New Feature 1.1
+            Feature1.1_AdminPages
+            Feature1.1_UserPages
+            Feature1.1_OperatorPages
+            New more branch - Agent Concept
+              Added AgentPages
+                This is a checkout a new function b and this will replace function a based on AgentPages.
+                Work on function b.0.0.1 but we found that function a is better solution.
+                  Good idea on this story as function b.0.0.2
+                    Updated function b.0.0.2.1
+
+>>>>>>> Stashed changes
         </p>
         <a
           className="App-link"
