5e1146db39dd60b52ac077cc02632eadd4f05e61
Winson Li <winsonliwh@gmail.com>
2022-01-09 12:55:05 +0800

    Move data to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,27 @@ import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import SearchBar from "../SearchBar/SearchBar";
 
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
+
+const businesses = [business, business, business, business, business, business];
+
 function App() {
   return (
     <div className="App">
 {/*# Below three line should be replaced below header tag before last div of className="App"*/}
       <h1>ravenous</h1>
       <SearchBar />
-      <BusinessList />
+      <BusinessList businesses={businesses}/>
 {/*
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -6,24 +6,11 @@ class BusinessList extends React.Component {
   render() {
     return (
       <div className="BusinessList">
-        <Business />
-        <Business />
-        {/* Add four more Business components} */}
-        <Business />
-        <Business />
-        {/* Add four more Business components} */}
-        <Business />
-        <Business />
-        {/* Add four more Business components} */}
-        <Business />
-        <Business />
-        {/* Add four more Business components} */}
-        <Business />
-        <Business />
-        {/* Add four more Business components} */}
-        <Business />
-        <Business />
-        {/* Add four more Business components} */}
+        {
+    this.props.businesses.map( business => {
+      return <Business business={business}/>;
+    });  
+  }
       </div>
     );
   }
