a158d0fc96df181b00c09d87ba4a43fe85c1ec38
Winson Li <winsonliwh@gmail.com>
2022-01-09 13:10:32 +0800

    Add comment for modifying props in Business.js

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -2,7 +2,27 @@ import React from 'react';
 import './Business.css';
 
 
+/*
+# Modifying Business.js using Props
 
+
+Let’s review what you’ve done so far:
+
+Moved business information to App.js
+Created a list of businesses using an array, rather than repeating components in BusinessList.js
+Built functionality for the BusinessList component to iterate through the array and return Business components
+You’re just about done! With the changes you’ve made, however, the Business component will have to access business information differently. This is what you’ll build next.
+
+Open Business.js. What do you notice about how information is accessed inside of the return statement?
+
+Because the business object was removed, statements like the following no longer have any meaning:
+- Before
+  {business.imageSrc}
+- After 
+  {this.props.business.imageSrc}
+
+Business information now has to be accessed via the business prop you set in BusinessList.js. Modify all statements (like the one in the example above) by prepending them with this.props.
+*/
 class Business extends React.Component {
   render() {
     return (
